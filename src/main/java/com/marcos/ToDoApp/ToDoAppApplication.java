893611f42cb303bf74e1e46e89d8f63cfe7e32c0
Marcos Rivero <marcos.rivero@dice40consultores.com>
2022-09-27 18:41:43 -0400

    Initial

---
+++ src/main/java/com/marcos/ToDoApp/ToDoAppApplication.java
@@ -0,0 +1,13 @@
+package com.marcos.ToDoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcos/ToDoApp/ToDoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.ToDoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
